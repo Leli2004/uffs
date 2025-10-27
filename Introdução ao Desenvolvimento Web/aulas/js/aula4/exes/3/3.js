2a220cee90863e3d14c27a0a79ed3fa3c0146704
Danieli NDV <danielidondallavecchia@gmail.com>
2025-10-27 18:38:06 -0300

    feat: Exe 03 aula 4

--- Introdução ao Desenvolvimento Web/aulas/js/aula4/exes/3/3.js
+++ Introdução ao Desenvolvimento Web/aulas/js/aula4/exes/3/3.js
@@ -2,3 +2,17 @@
 3) Escreva uma página que aumente o tamanho de um Emoji quando pressionar a seta para cima (arrowUp), e
 diminua de tamanho quando pressionar a tecla para baixo (arrowDown).
 */
+
+const emoji = document.getElementById("emoji");
+let tamanho = emoji.width;
+
+function changeEmoji(e) {
+    if (e.key === "ArrowUp") {
+        tamanho += 10;
+    } else if (e.key === "ArrowDown") {
+        tamanho -= 10;
+    }
+    emoji.width = tamanho;
+};
+
+document.addEventListener("keydown", (e) => changeEmoji(e));
