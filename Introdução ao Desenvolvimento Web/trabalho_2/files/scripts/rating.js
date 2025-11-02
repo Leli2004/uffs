b8c6e279fad80dfbec6ef88cc2ba42e0818f6e0f
Danieli NDV <danielidondallavecchia@gmail.com>
2025-11-02 01:10:48 -0300

    feat: Fake envio de avaliação do filme

---
+++ Introdução ao Desenvolvimento Web/trabalho_2/files/scripts/rating.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const addReviewBtn = document.getElementById('add-review-btn');
+  const reviewForm = document.getElementById('review-form');
+  const reviewMessage = document.getElementById('review-message');
+
+  addReviewBtn.addEventListener('click', () => {
+    addReviewBtn.style.display = 'none';
+    reviewForm.style.display = 'block';
+    reviewMessage.textContent = '';
+  });
+
+  reviewForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = reviewForm.email.value.trim();
+
+    // Exibe mensagem de sucesso e substitui o formulário
+    reviewForm.style.display = 'none';
+    reviewMessage.textContent = `Avaliação enviada com sucesso! \nVerifique seu email "${email}" para confirmar.`;
+  });
+});
