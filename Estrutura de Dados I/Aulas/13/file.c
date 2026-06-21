eb03681f595777279a712fe4bf74ce1355710ecb
Danieli NDV <danielidondallavecchia@gmail.com>
2026-06-21 16:37:31 -0300

    feat: ED Aula 13 - Implementa remoção em heap

--- Estrutura de Dados I/Aulas/13/file.c
+++ Estrutura de Dados I/Aulas/13/file.c
@@ -11,6 +11,10 @@ struct TMaxHeap{
 };
 typedef struct TMaxHeap MaxHeap;
 
+void initMaxHeap(MaxHeap *h) {
+	h->size=0;
+}
+
 // Troca valores de ponteiros a e b
 void swap(int *a, int *b) {
     if (a == NULL || b == NULL) return;
@@ -41,13 +45,57 @@ void insereHeapMax(MaxHeap *h, int v) {
 // Remove elemento (sempre remove o maior)
 int removeMaxHeap(MaxHeap *h) {
     if (h->size == 0) return -1;
-    int removed = h->heap[0];
-    
-    // todo: implementar
 
-    return removed;
+    int maior;
+    int v, i, left, right;
+    
+    v = h->heap[0];
+    
+    h->heap[0] = h->heap[h->size - 1];
+    h->size--;
+    i = 0;
+
+    while(i < h->size) {
+        //printf("%d\n",maior);
+        left =(2*i)+1;
+        right =(2*i)+2;
+
+        if (left < h->size && h->heap[i] < h->heap[left]) maior=left;;
+
+        if (right < h->size && h->heap[right] > h->heap[left]) maior=right;
+
+        swap (&h->heap[i], &h->heap[maior]);
+
+        if (i==maior) break;
+        i=maior;
+    }
+
+    return v;
+}
+
+// Exibe o heap
+void printHeap(MaxHeap h) {
+    printf("\nHeap:\n");
+    for (int i=0; i<h.size; i++)
+	   printf("%d ",h.heap[i]);
+    printf("\n");
 }
 
 int main() {
+    MaxHeap h;
+	initMaxHeap(&h);
+	insereHeapMax(&h,40);
+	insereHeapMax(&h,20);
+	insereHeapMax(&h,50);
+	insereHeapMax(&h,30);
+	insereHeapMax(&h,45);
+	insereHeapMax(&h,60);
+
+	printHeap(h);
+
+    printf("\nValor max removido: %d\n", removeMaxHeap(&h));
+
+	printHeap(h);
+
     return 0;
 }
